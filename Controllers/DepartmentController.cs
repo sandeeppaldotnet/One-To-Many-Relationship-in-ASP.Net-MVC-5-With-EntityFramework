e5f62580cc13b31df835f3a1b3e99c68b4b5472e
sandeep084991904 <sandeep084991904@gmail.com>
2023-06-29 13:03:47 +0530

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly MVC_DBEntities1 _dbContext;
+         public DepartmentController()
+        {
+            _dbContext = new MVC_DBEntities1();
+        }
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            List<Department> listDepartment=_dbContext.Departments.ToList();
+            return View(listDepartment);
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly MVC_DBEntities1 _dbContext;
+        // GET: Employee
+
+        public EmployeeController()
+        {
+            _dbContext = new MVC_DBEntities1();
+        }
+        public ActionResult Index(int departmentId)
+        {
+
+            List<Employee>listEmployee=_dbContext .Employees.Where(e=>e.DepartmentId == departmentId).ToList();
+            return View(listEmployee);
+        }
+        public ActionResult Details(int id) {    
+         Employee employee=_dbContext.Employees.Where(e=>e.EmployeeId==id).FirstOrDefault();
+         return View(employee);
+        }
+    }
+}
